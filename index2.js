a55db984df68a324de2248a46f4daf9049283890
Siam-al-islam <siamalislam692@gmail.com>
2025-02-16 19:04:37 +0600

    Refactor index2.js to use an IIFE and improve code structure

--- index2.js
+++ index2.js
@@ -1,12 +1,14 @@
 // how node js works:
-(function (exports, require, module, __filename, __dirname) {
-    // code here
-    const people = ['John', 'Doe', 'Jane', 'Doe'];
+// IIFE (Immediately Invoked Function Expression) - this function will be executed immediately after it has been defined (it will not be stored in memory) and it will not be accessible from the outside.
 
-    let a = 4;
-    function test() {
-        console.log("test");
-    }
+// (function (exports, require, module, __filename, __dirname) {
+const people = ['John', 'Doe', 'Jane', 'Doe'];
 
-    module.exports = people;
-})(); // IIFE (Immediately Invoked Function Expression) - this function will be executed immediately after it has been defined (it will not be stored in memory) and it will not be accessible from the outside.
+let a = 4;
+function test() {
+    console.log("test");
+}
+
+module.exports = people;
+// return module.exports    ;
+// }); 
