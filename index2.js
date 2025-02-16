2d9e81e9b81e6e8c1153b86b9a6719926238fe3f
Siam-al-islam <siamalislam692@gmail.com>
2025-02-16 18:57:55 +0600

    Add IIFE (Immediately Invoked Function Expression) to index2.js

--- index2.js
+++ index2.js
@@ -5,3 +5,7 @@ function test() {
     console.log("test");
 }
 module.exports = people;
+
+(function () {
+    var a = 3;
+})(); // IIFE (Immediately Invoked Function Expression) - this function will be executed immediately after it has been defined (it will not be stored in memory) and it will not be accessible from the outside.
