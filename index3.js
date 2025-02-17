88d3b18eb30b90a0e2e1baa0b472bf573083b87f
Siam-al-islam <siamalislam692@gmail.com>
2025-02-17 21:09:48 +0600

    Add path, os, and fs modules to index3.js

--- index3.js
+++ index3.js
@@ -1,7 +1,23 @@
 const path = require('path');
+const os = require('os');
+const fs = require('fs');
 const myPath = 'D:/Web Development/node.js/index3.js';
 
 console.log(path.basename(myPath));
 console.log(path.dirname(myPath));
 console.log(path.extname(myPath));
 console.log(path.parse(myPath));
+
+console.log(os.platform());
+console.log(os.homedir());
+console.log(os.freemem());
+console.log(os.type());
+console.log(os.cpus());
+console.log(os.version());
+
+fs.writeFileSync('test.txt', 'Hello Programmers');
+fs.appendFileSync('test.txt', ' How are you?');
+
+fs.readFile('test.txt', (err, data) => {
+    console.log(data.toString());
+});
