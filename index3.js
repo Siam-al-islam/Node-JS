a22a72899a88c2fae5545c8c0242068ea09db9bc
Siam-al-islam <siamalislam692@gmail.com>
2025-02-18 19:03:37 +0600

    Add event emitter to index3.js

--- index3.js
+++ index3.js
@@ -1,23 +1,36 @@
 const path = require('path');
 const os = require('os');
 const fs = require('fs');
+const EventEmitter = require('events');
 const myPath = 'D:/Web Development/node.js/index3.js';
 
-console.log(path.basename(myPath));
-console.log(path.dirname(myPath));
-console.log(path.extname(myPath));
-console.log(path.parse(myPath));
+// console.log(path.basename(myPath));
+// console.log(path.dirname(myPath));
+// console.log(path.extname(myPath));
+// console.log(path.parse(myPath));
 
-console.log(os.platform());
-console.log(os.homedir());
-console.log(os.freemem());
-console.log(os.type());
-console.log(os.cpus());
-console.log(os.version());
+// console.log(os.platform());
+// console.log(os.homedir());
+// console.log(os.freemem());
+// console.log(os.type());
+// console.log(os.cpus());
+// console.log(os.version());
 
-fs.writeFileSync('test.txt', 'Hello Programmers');
-fs.appendFileSync('test.txt', ' How are you?');
+// fs.writeFileSync('test.txt', 'Hello Programmers');
+// fs.appendFileSync('test.txt', ' How are you?');
 
-fs.readFile('test.txt', (err, data) => {
-    console.log(data.toString());
+// fs.readFile('test.txt', (err, data) => {
+//     console.log(data.toString());
+// });
+
+const emitter = new EventEmitter();
+
+// register a listener for bellRing event
+emitter.on('bellRing', (period) => {
+    console.log(`ring ring ring, ${period}`);
 });
+
+// raise an event
+setTimeout(() => {
+    emitter.emit('bellRing', 'second period is over');
+}, 2000);
