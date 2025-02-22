1d889c8e65fa1885c6f530fe6408b5f188d4f58b
Siam-al-islam <siamalislam692@gmail.com>
2025-02-22 21:18:28 +0600

    Refactor index.js to use fs module for file streaming and add write stream

--- lesson-6/index.js
+++ lesson-6/index.js
@@ -1,38 +1,8 @@
-// const fs = require('fs');
+const fs = require('fs');
 
-// const ourReadStream = fs.createReadStream(`${__dirname}/data.txt`);
+const ourReadStream = fs.createReadStream(`${__dirname}/data.txt`);
+const ourWriteStream = fs.createWriteStream(`${__dirname}/output.txt`);
 
-// ourReadStream.on('data', (chunk) => {
-//     console.log(chunk.toString());
-// });
-
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.write('<html><head><title></title></head>');
-        res.write('<body><form method="post" action="/process"><input name="message"></input></form></body>');
-        res.end();
-    }
-    else if (req.url === '/process' && req.method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            body.push(chunk);
-        });
-        req.on('end', () => {
-            console.log('Stream Finished');
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(parsedBody);
-        });
-        res.write('Thank you for your message');
-        res.end();
-    }
-    else {
-        res.write('404 page not found');
-        res.end();
-    }
+ourReadStream.on('data', (chunk) => {
+    ourWriteStream.write(chunk);
 });
-
-server.listen(3000, () => {
-    console.log('Server is running at 3000');
-})
