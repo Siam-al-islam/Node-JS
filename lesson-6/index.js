6e20d6deeb9967bbfde115fdc95ed883600aa1c5
Siam-al-islam <siamalislam692@gmail.com>
2025-02-22 21:29:02 +0600

    Refactor file streaming in index.js to use pipe method

--- lesson-6/index.js
+++ lesson-6/index.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const ourReadStream = fs.createReadStream(`${__dirname}/data.txt`);
 const ourWriteStream = fs.createWriteStream(`${__dirname}/output.txt`);
 
-ourReadStream.on('data', (chunk) => {
-    ourWriteStream.write(chunk);
-});
+// ourReadStream.on('data', (chunk) => {
+//     ourWriteStream.write(chunk);
+// });
+
+ourReadStream.pipe(ourWriteStream);
